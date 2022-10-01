a7a0b2eab4895f27ff1758c0d4496ac3033d4261
vdv <vdv@gmail.com>
2022-10-01 22:12:11 +0500

    first program created

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def print_hello(name):
+    print('Hello ' + name)
+
+
+if __name__ == '__main__':
+    # name = 1234
+    print_hello(str(1234))
