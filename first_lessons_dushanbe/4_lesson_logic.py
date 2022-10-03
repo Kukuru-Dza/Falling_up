53438f56a6bf831bc2ad316920837bd1094d6f64
vdv <vdv@gmail.com>
2022-10-03 23:34:32 +0500

    first program created

--- first_lessons_dushanbe/4_lesson_logic.py
+++ first_lessons_dushanbe/4_lesson_logic.py
@@ -11,7 +11,7 @@ def has_upper_case(p):
 if __name__ == '__main__':
     pass_word = input(str('Enter your password. At least one letter should be capital: '))
     if bool(has_upper_case(pass_word)):
-        print('True')
+        print('Got it')
     else:
         print("Not acceptable")
 
