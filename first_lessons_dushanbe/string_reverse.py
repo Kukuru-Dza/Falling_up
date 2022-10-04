639a458b19f4ceb78891dcfd156c1866d5b92832
vdv <vdv@gmail.com>
2022-10-04 11:29:43 +0500

    first program created

---
+++ first_lessons_dushanbe/string_reverse.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#def reversing(p):
+
+
+
+if __name__ == '__main__':
+    string_to_reverse = input(str('Enter a string to be reversed: '))
+    string_lenght = len(string_to_reverse)
+    remaining_operation = 0
+    while remaining_operation < string_lenght:
+        print(string_to_reverse[(-1-remaining_operation)], end="")
+        remaining_operation = remaining_operation+1
