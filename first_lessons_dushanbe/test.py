52260f4cacac2df01f14253c51512f7a8895b816
vdv <vdv@gmail.com>
2022-10-06 10:05:39 +0500

    new function applied / changed logic

---
+++ first_lessons_dushanbe/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def normalize_url(a):
+    if a[:8] == 'https://':
+        print(a)
+    elif a[:7] == 'http://':
+        print('https://'+a[7:-1])
+    else:
+        print('https://'+a)
+
+
+if __name__ == '__main__':
+    link = 'twitter.com'
+    normalize_url(link)
+    link = 'https://ya.ru'
+    normalize_url(link)
+    link = 'http://google.com'
+    normalize_url(link)
