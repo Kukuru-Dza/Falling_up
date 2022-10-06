1347a10d9c71bc52908b3b4cf2c1114c77331e96
vdv <vdv@gmail.com>
2022-10-06 10:48:00 +0500

    new function applied / changed logic

---
+++ first_lessons_dushanbe/Тестовые задания из уроков/test_while.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def print_numbers(a):
+    while a > 0:
+        print(a)
+        a -= 1
+
+
+if __name__ == '__main__':
+    number = 5
+    print_numbers(number)
+
+
