07c233baba4f2e5b9804103ea386b02f54b5434d
vdv <vdv@gmail.com>
2022-10-08 12:43:10 +0500

    задание у уроку

--- first_lessons_dushanbe/define_count_and_index.py
+++ first_lessons_dushanbe/define_count_and_index.py
@@ -17,25 +17,12 @@ def fn_index(a, b):
     if a is None or b is None:
         return False
     x = 0
-    count = 0
-    while x < len(a) and count == 0:
+    while x < len(a):
         if b == a[x]:
-            count += 1
             return x
-        else:
-            x += 1
+        x += 1
     return False
 
 
 if __name__ == '__main__':
-    print(fn_count([0, 2, 3, 2, 5, 2, 7], 2))
-    print(fn_count([0, 2, 3, 2, 5, 2, 7], 5))
-    print(fn_count([0, 2, 3, 2, 5, 2, 7], 8))
-    print(fn_count([0, 2, 3, 2, 5, 2, 7], None))
-    print(fn_count(None, 2))
-    print('')
-    print(fn_index([0, 2, 3, 2, 5, 2, 7], 2))
-    print(fn_index([0, 2, 3, 2, 5, 2, 7], 5))
-    print(fn_index([0, 2, 3, 2, 5, 2, 7], 8))
-    print(fn_index([0, 2, 3, 2, 5, 2, 7], None))
-    print(fn_index(None, 2))
+    print(fn_index([0, 2, 3, 5, 7], 2))
