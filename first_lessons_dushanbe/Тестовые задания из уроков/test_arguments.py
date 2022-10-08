dd4ef8bbd9d81e851271655c757de404cf3524c9
vdv <vdv@gmail.com>
2022-10-08 10:54:40 +0500

    задание у уроку

---
+++ first_lessons_dushanbe/Тестовые задания из уроков/test_arguments.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def is_contains_char(a, b):
+    count = 0
+    x = 0
+    while count < len(a) or x == 1:
+        if b == a[count] or b == a[count].upper or b == a[count].lower:
+            x += 1
+        count += 1
+
+    if x > 0:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(is_contains_char('The Tallest tree in the world', 'e'))
