de1c296fb10db61fe236beb0607602983dd0a98f
vdv <vdv@gmail.com>
2022-10-08 09:41:14 +0500

    написал функции и тест к ней. Но не могу разобраться почему тесты выполняются по отдельности и их 8 штук, а если запустить все сразу то пишет что выполнено 6 тестов

--- first_lessons_dushanbe/test_count_index.py
+++ first_lessons_dushanbe/test_count_index.py
@@ -3,6 +3,8 @@
 from unittest import TestCase
 from define_count_and_index import fn_count
 from define_count_and_index import fn_index
+
+
 class LessonArrayTest(TestCase):
     def test_count_item_not_found(self):
         a = [0, 2, 3, 5, 7]
@@ -10,21 +12,52 @@ class LessonArrayTest(TestCase):
         expected = 0
         got = fn_count(a, b)
         self.assertEqual(got, expected, 'function should return -1 if not found')
+
     def test_count_item_found(self):
         a = [0, 2, 3, 5, 5, 5, 5, 7]
         b = 5
-        expected = 3
+        expected = 4
         got = fn_count(a, b)
         self.assertEqual(got, expected, f'function should return index ({expected}) but got {got}')
+
     def test_count_a_is_none(self):
         a = None
         b = 3
         expected = 0
         got = fn_count(a, b)
         self.assertEqual(got, expected, 'function should return 0 if not found')
+
     def test_count_b_is_none(self):
         a = [0, 2, 3, 5, 7]
         b = None
         expected = 0
         got = fn_count(a, b)
+        self.assertEqual(got, expected, 'function should return 0 if not found')
+
+    def test_index_item_not_found(self):
+        a = [0, 2, 3, 5, 7]
+        b = 77
+        expected = False
+        got = fn_index(a, b)
+        self.assertEqual(got, expected, 'function should return -1 if not found')
+
+    def test_index_item_found(self):
+        a = [0, 2, 3, 5, 5, 5, 5, 7]
+        b = 3
+        expected = 2
+        got = fn_index(a, b)
+        self.assertEqual(got, expected, f'function should return index ({expected}) but got {got}')
+
+    def test_count_a_is_none(self):
+        a = None
+        b = 3
+        expected = False
+        got = fn_index(a, b)
+        self.assertEqual(got, expected, 'function should return 0 if not found')
+
+    def test_count_b_is_none(self):
+        a = [0, 2, 3, 5, 7]
+        b = None
+        expected = False
+        got = fn_index(a, b)
         self.assertEqual(got, expected, 'function should return 0 if not found')
