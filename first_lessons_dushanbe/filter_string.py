fb6d2cc753166d2177a8ba26ce4eaafa62b10353
vdv <vdv@gmail.com>
2022-10-10 11:47:04 +0500

    тест к define_count написал, все работает (включая то что дополнительно надо было - элемент массива текстовый и элемент массива None

--- first_lessons_dushanbe/filter_string.py
+++ first_lessons_dushanbe/filter_string.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 
 def filter_string(a, b):
+    if a is None:
+        return False
     output_string = ''
-    if a[0] == ' ':
+    while a[0] == ' ':
         a = a[1:]
-    if a[-1] == ' ':
+    while a[-1] == ' ':
         a = a[:-1]
+    if b is None:
+        return a
     count = 0
-    for symbol in a:
+    for _ in a:
         if b.lower() != a[count].lower():
             output_string += a[count]
         count += 1
@@ -16,6 +20,6 @@ def filter_string(a, b):
 
 
 if __name__ == '__main__':
-    text = ' If I look forward I win '
+    text = '       If I look forward I win       '
     print(filter_string(text, 'i') + '!')  # 'f  look forward  wn'
     print(filter_string(text, 'O') + '!')  # 'If I lk frward I win
