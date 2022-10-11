5695e8e09476f57cf4df383571f7b63e3d22180a
vdv <vdv@gmail.com>
2022-10-11 11:27:23 +0500

    test_array_sorting doesnt work!!!

--- first_lessons_dushanbe/array_sorting.py
+++ first_lessons_dushanbe/array_sorting.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
-def none_check(a):
-    if a is None:
-        return
-
-
 def list_sort(a):
-    if not none_check(a):
-        return
+    if a is None:
+        return False
     lst = []
     ln = len(a)
     while len(lst) < ln:
@@ -23,6 +17,6 @@ def list_sort(a):
 
 
 if __name__ == '__main__':
-    #arr = [-4, 6, 3, 7, 22, 1, 1, 1, -1]
     arr = None
+    #arr = [-4, 6, 3, 7, 22, 1, 1, 1, -1]
     print(list_sort(arr))
