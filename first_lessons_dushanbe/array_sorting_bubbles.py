4cb7ace0fe79ece3cdc742c577a2ee75a95f9d56
vdv <vdv@gmail.com>
2022-10-13 10:32:08 +0500

    - sorting by changing values 1 вариант что минимальное ползет вниз 2 вариант что максимальное ползет вверх.

--- first_lessons_dushanbe/array_sorting_bubbles.py
+++ first_lessons_dushanbe/array_sorting_bubbles.py
@@ -5,14 +5,13 @@
 def list_sort(a):
     if a is None:
         return a
-    count = 0
-    while count < len(a)-1:
-        ln = len(a) - 1
-        while ln > count:
-            if a[ln] < a[ln-1]:
-                (a[ln], a[ln-1]) = (a[ln-1], a[ln])
-            ln -= 1
-        count += 1
+    count = len(a)-1
+    while count > 1:
+        ln = 0
+        for ln in range(count):
+            if a[ln] > a[ln+1]:
+                (a[ln+1], a[ln]) = (a[ln], a[ln+1])
+        count -= 1
     return a
 
 if __name__ == '__main__':
