b4b8c03743bae6960804e90010b7fbbd919550b2
vdv <vdv@gmail.com>
2022-10-18 18:29:37 +0500

    trying to implement binary search

--- first_lessons_dushanbe/array_sorting_binary_search.py
+++ first_lessons_dushanbe/array_sorting_binary_search.py
@@ -18,19 +18,17 @@ def list_sort(a):
 def bin_index(s_a, i):
     if i is None or s_a is None:
         return -1
-    elif i > s_a[-1]:
-        return -1
-    elif i < s_a[0]:
-        return -1
     elif len(s_a) == 1:
         return 0
     elif i not in s_a:
         return -1
+    elif i == s_a[-1]:
+        return len(s_a) - 1
 
     mn = 0
-    mx = len(s_a)-1
-    mid = (mn + (mx-mn)) // 2
-    while mn <= mx:
+    mx = len(s_a) - 1
+    mid = (mn + mx) // 2
+    while mn < mx:
         if i == s_a[mid]:
             return mid
         elif i < s_a[mid]:
@@ -38,11 +36,12 @@ def bin_index(s_a, i):
         elif i > s_a[mid]:
             mn = mid + 1
         mid = (mn + (mx-mn)) // 2
+    return mid
 
 
 
 if __name__ == '__main__':
     arr = [5, 4, 3, 2, 1]
     arr_sorted = list_sort(arr)
-    ind = 4
+    ind = 3
     print(bin_index(arr_sorted, ind))
