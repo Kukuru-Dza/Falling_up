a23bee01001c9915760dcaf33f7a2435fcc983a4
vdv <vdv@gmail.com>
2022-10-19 11:25:41 +0500

    BINARY SEARCH WORKS FINE WITH ALL DIFFERENT NUMBERS IN ARRAY, BUT DOESN'T WORK CORRECTLY WITH REPEATED NUMBER
    TEST NOT READY.

--- first_lessons_dushanbe/array_sorting_binary_search.py
+++ first_lessons_dushanbe/array_sorting_binary_search.py
@@ -8,32 +8,29 @@ from array_sorting_bubbles import list_sort
 def bin_index(s_a, i):
     if i is None or s_a is None:
         return -1
-    elif len(s_a) == 1:
-        return 0
-    elif i not in s_a:
+    elif i < s_a[0] or i > s_a[-1]:
         return -1
-    elif i == s_a[-1]:
-        return len(s_a) - 1
-
     first = 0
     last = len(s_a) - 1
-    mid = (first + last) // 2
+    if i == s_a[first]:
+        return first
+    elif i == s_a[last]:
+        return last
     while first < last:
+        mid = (first + last) // 2
         if i == s_a[mid]:
             return mid
         elif i < s_a[mid]:
-            last = mid - 1
-        else:
-            i > s_a[mid]
-        first = mid + 1
-        mid = (first + (last - first)) // 2
-    return mid
+            last = mid
+        elif i > s_a[mid]:
+            first = mid
+
 
 
 
 if __name__ == '__main__':
-    arr = [5, 4, 3, 2, 1]
+    arr = [-4, 6, 3, 7, 22, 1, 1, 1, -1]
     arr_sorted = list_sort(arr)
     print(arr_sorted)
-    ind = 4
+    ind = 1
     print(bin_index(arr_sorted, ind))
