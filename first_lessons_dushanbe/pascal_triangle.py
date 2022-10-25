15c9505124677b5ed4b7550c22c4c42caba8fdd2
vdv <vdv@gmail.com>
2022-10-25 14:53:25 +0500

    поигрался

--- first_lessons_dushanbe/pascal_triangle.py
+++ first_lessons_dushanbe/pascal_triangle.py
@@ -5,15 +5,14 @@
 def pascal_tr(b):
     a = []
     for i in range(b + 1):
-        a.append([0] * (b + 1))
-    for i in range(b + 1):
-        a[i][0] = 1
+        a.append([1] + [0] * b)
+
     for i in range(1, (b + 1)):
-        for j in range(1, (b + 1)):
+        for j in range(1, (i + 1)):
             a[i][j] = a[i - 1][j - 1] + a[i - 1][j]
     return a[b]
 
 
 if __name__ == '__main__':
-    a = 3
+    a = 10
     print(pascal_tr(a))
