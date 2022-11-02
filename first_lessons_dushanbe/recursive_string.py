26a75f04ea4e63f318da159a20f44564695b1b94
vdv <vdv@gmail.com>
2022-11-02 15:53:26 +0500

    study

--- first_lessons_dushanbe/recursive_string.py
+++ first_lessons_dushanbe/recursive_string.py
@@ -8,13 +8,19 @@ def rec_str(a):
     return a
 
 def rec_str_recurs(a):
+    if len(a) == 1 or len(a) == 2:
+        return a
+    else:
+        return a[0] + '(' + rec_str_recurs(a[1:-1]) + ')' + a[-1]
 
 
 
 if __name__ == '__main__':
-    st = list(input('Enter: '))
-    if len(st) >= 100:
+    st = input('Enter: ')
+    st1 = list(st)
+    if len(st1) >= 100:
         res = -1
     else:
-        res = ''.join(rec_str(st))
+        res = ''.join(rec_str(st1))
     print(res)
+    print(rec_str_recurs(st))
