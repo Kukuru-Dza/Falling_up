7fc0c7b46004868bcbbaa13813e3448679523601
vdv <vdv@gmail.com>
2022-11-10 11:16:49 +0500

    study

--- first_lessons_dushanbe/tasks_from_lessons/count_numbers_in_string.py
+++ first_lessons_dushanbe/tasks_from_lessons/count_numbers_in_string.py
@@ -4,24 +4,24 @@
 
 
 def count(x):
-    res = [0 for ii in range(10)]
-    for k in range(len(x)):
-        el = int(x[k])
+    b = list(x)
+    dig_list = []
+    for i in range(len(b)):
+        if b[i].isdigit():
+            dig_list.append(b[i])
+    res = [0 for _ in range(10)]
+    for k in range(len(dig_list)):
+        el = int(dig_list[k])
         res[el] += 1
     return res
 
 
 if __name__ == '__main__':
     a = 'oiwdoie09 31028u9wr8024978[wd ohcsludgfiwytdajscbow8eyr2 it3riqeyr0qogevj23ri2 uyfpsdjcsl'
-    b = list(a)
-    dig_list = []
-    for i in range(len(b)):
-        if b[i].isdigit():
-            dig_list.append(b[i])
-    y = count(dig_list)
-    for i in range(10):
-        if y[i] > 0:
-            print(i, ':', y[i], end='; ')
+    y = count(a)
+    for j in range(10):
+        if y[j] > 0:
+            print(j, ':', y[j], end='; ')
     print()
     print(dict(enumerate(y))) #Словарь
     print(list(enumerate(y))) #Список
